d65d93d9a3d12d0880f4298bf252a2b5220e2dd8
metalblueberry <metalblueberry@gmail.com>
2024-03-10 14:21:21 +0100

    fix posix source

--- config/fish/functions/posix-source.fish
+++ config/fish/functions/posix-source.fish
@@ -1,7 +1,7 @@
 function posix-source
 	for i in (cat $argv | grep -v "^#" | grep =)
         echo $i
-		set arr (echo $i |tr = \n)
+		set arr (string split "=" -m 1 $i)
   		set -gx $arr[1] $arr[2]
 	end
 end
