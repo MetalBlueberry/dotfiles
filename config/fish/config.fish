b92b9f1d847b0e6bee54eef8f4582d293ff09347
metalblueberry <metalblueberry@gmail.com>
2024-08-01 12:32:11 +0200

    remove the office

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,7 @@ set -x EDITOR vim
 set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
 set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
 set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+set -q PATH; or set PATH ''; set -gx PATH "/home/victor/Applications" "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
 set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
 set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
 
@@ -13,4 +13,3 @@ set -gx AWS_CA_BUNDLE /etc/ssl/certs/ca-certificates.crt
 set -gx SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
 set -gx NODE_EXTRA_CA_CERTS /etc/ssl/certs/ca-certificates.crt
 
-set -gx OFFICE "https://form3.zoom.us/j/92341007502?pwd=bGlHTmprREY2YWhPTUNlcG9JMERMZz09"
