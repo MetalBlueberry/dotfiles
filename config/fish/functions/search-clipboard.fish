2b007e3bc1d77cc34df509a8e750d4d199f95e2d
Victor Perez <vperez@timescale.com>
2025-03-18 14:54:30 +0100

    save

--- config/fish/functions/search-clipboard.fish
+++ config/fish/functions/search-clipboard.fish
@@ -1,3 +1,13 @@
 function search-clipboard
-	greenclip print | grep . | fzf -e --preview-window right:wrap --preview='echo {} | grep --color=always {q}' | xargs -r -d'\n' -I '{}' greenclip print '{}'       
+	set -l tmpfile (mktemp)
+
+	greenclip print | grep . | fzf --preview-window right:wrap --preview='echo {} | grep -i --color=always {q}'  > $tmpfile
+
+	# Copy edited content back to clipboard
+	greenclip print (cat $tmpfile)
+	xsel -b | xsel -bi
+	xsel -b | xsel -pi
+
+	rm $tmpfile
+	
 end
