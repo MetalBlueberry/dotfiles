00fa0babefe0c101178d38387e889f9ea9ada640
metalblueberry <metalblueberry@gmail.com>
2025-03-29 17:03:12 +0100

    update

--- config/fish/functions/search-clipboard.fish
+++ config/fish/functions/search-clipboard.fish
@@ -1,6 +1,4 @@
 function search-clipboard
-	blueclip client list | fzf --read0 --print0 --bind 'change:first' --preview-window right:wrap --preview 'echo {} | blueclip client print -u' | blueclip client copy -c primary -c clipboard
-	return
-	blueclip client list | fzf --read0 --bind 'change:first' --preview-window right:wrap --preview 'echo {} | blueclip client print -u' | blueclip
-	xsel -b | xsel -pi
+	blueclip client list | fzf --gap --read0 --print0 --bind 'change:first'  | blueclip client copy -c primary -c clipboard
+	# --preview-window right:wrap --preview 'echo {} | blueclip client print -u'
 end
